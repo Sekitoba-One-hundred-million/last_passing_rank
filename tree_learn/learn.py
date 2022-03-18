1e1fe7c3c8501b1508261f9f8f6c229130afd529
kansei <kansei@kansei.local>
2022-03-18 21:00:58 +0900

    MPI enable

--- tree_learn/learn.py
+++ tree_learn/learn.py
@@ -16,13 +16,10 @@ import pandas as pd
 import sekitoba_library as lib
 import sekitoba_data_manage as dm
 
-check_year = 2020
-
 def lg_main( data, params = None ):
     max_pos = np.max( np.array( data["answer"] ) )
     lgb_train = lgb.Dataset( np.array( data["teacher"] ), np.array( data["answer"] ) )
     lgb_vaild = lgb.Dataset( np.array( data["test_teacher"] ), np.array( data["test_answer"] ) )
-
 
     if params == None:
         lgbm_params =  {
@@ -44,7 +41,7 @@ def lg_main( data, params = None ):
     
     #print( df_importance.head( len( x_list ) ) )
     dm.pickle_upload( "last_horce_body_lightbgm_model.pickle", bst )
-    lib.log.write_lightbgm( bst )
+    #lib.log.write_lightbgm( bst )
     return bst
 
 def lgb_test( data ):
@@ -79,8 +76,35 @@ def test( data, model ):
     count = 0
 
     for i in range( 0, len( predict_answer ) ):
-        diff += abs( predict_answer[i] - data["test_answer"][i] )
+        pah = max( predict_answer[i], 0 )
+        pah = int( pah * 2 ) / 2
+        diff += abs( pah - data["test_answer"][i] )        
         count += 1
+
+    print( diff / count )
+
+def simu_test( simu_data, model ):
+    diff = 0
+    count = 0
+
+    for k in simu_data.keys():
+        data = []
+
+        for kk in simu_data[k].keys():
+            instance = {}
+            pah = max( model.predict( np.array( [ simu_data[k][kk]["data"] ] ) )[0], 0 )
+            pah = int( pah * 2 ) / 2
+            
+            instance["predict"] = pah
+            instance["answer"] = simu_data[k][kk]["answer"]
+            data.append( instance )
+
+        sort_list = sorted( data, key=lambda x:x["predict"] )        
+        
+        for i in range( 0, len( sort_list ) ):
+            pah = sort_list[i]["predict"] - sort_list[0]["predict"]
+            diff += abs( pah - sort_list[i]["answer"] )
+            count += 1
 
     print( diff / count )
 
@@ -96,7 +120,7 @@ def data_check( data ):
         current_data = data["teacher"][i]
         answer_horce_body = data["answer"][i]
 
-        if year == "2020":
+        if year == lib.test_year:
             result["test_teacher"].append( current_data )
             result["test_answer"].append( float( answer_horce_body ) )
         else:
@@ -105,8 +129,9 @@ def data_check( data ):
 
     return result
 
-def main( data ):
+def main( data, simu_data ):
     learn_data = data_check( data )
     #params = lgb_test( learn_data )
     model = lg_main( learn_data )
     test( learn_data, model )
+    simu_test( simu_data, model )
